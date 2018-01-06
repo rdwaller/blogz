252b9209417481cc79607a903ff43737896e437e
Ryan Waller <rdwaller@gmail.com>
2018-01-05 20:55:39 -0600

    updated draft of signin route and html

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:pal1d0@localhost:3306/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&zP3B'
 
 
 class User(db.Model):
@@ -18,7 +19,6 @@ class User(db.Model):
   def __init__(self, username, password):
     self.username = username
     self.password = password
-#   self.blogs = blogs
 
 class Blog(db.Model):
 
@@ -56,73 +56,85 @@ def signup():
         verify_password = request.form['verify_password']
         email = request.form['enter_email']
         existing_user = User.query.filter_by(username=username).first()
+        if not existing_user:
+            def validate_username(username):
+                if username == '':
+                    return False
+                elif len(username) < 3 or len(username) > 20:
+                    return False
+                elif (' ' in username) == True:
+                    return False
+                else:
+                    return True
+
+            def validate_password(password):
+                if password == '':
+                    return False
+                elif len(password) < 3 or len(password) > 20:
+                    return False
+                else:
+                    return True
+
+            def validate_email(email):
+                if len(email) == 1 or len(email) ==2 or len(email) > 20:
+                    return False
+                elif (' ' in email) == True:
+                    return False
+                elif ('@' in email) < 1 or ('@' in email) > 1:
+                    return False
+                elif ('.' in email) < 1 or ('.' in email) > 1:
+                    return False
+                else:
+                    return True
+
+            def password_match(verify_password, password):
+                if verify_password != password:
+                    return False
+                else:
+                    return True
+            
+            if validate_username(username) == False:
+                username_error = 'That is not a valid username.'
+            else:
+                username_error = ''
+
+            if validate_password(password) == False:
+                password_error = 'That is not a valid password.'
+            else:
+                password_error = ''   
+
+            if password_match(verify_password, password) == False:
+                verify_password_error = 'Passwords do not match.'
+            else:
+                verify_password_error = ''
+
+            if validate_email(email) == True or email == '':
+                email_error = ''
+            else:
+                email_error = 'That is not a valid email address.'
         
-        def validate_username(username):
-            if username == '':
-                return False
-            elif len(username) < 3 or len(username) > 20:
-                return False
-            elif (' ' in username) == True:
-                return False
-            else:
-                return True
-
-        def validate_password(password):
-            if password == '':
-                return False
-            elif len(password) < 3 or len(password) > 20:
-                return False
-            else:
-                return True
-
-        def validate_email(email):
-            if len(email) == 1 or len(email) ==2 or len(email) > 20:
-                return False
-            elif (' ' in email) == True:
-                return False
-            elif ('@' in email) < 1 or ('@' in email) > 1:
-                return False
-            elif ('.' in email) < 1 or ('.' in email) > 1:
-                return False
-            else:
-                return True
-
-        def password_match(verify_password, password):
-            if verify_password != password:
-                return False
-            else:
-                return True
-        
-        if validate_username(username) == False:
-            username_error = 'That is not a valid username.'
+            if validate_username(username) == True and validate_password(password) == True and password_match(verify_password, password)==True and (validate_email(email) == True or email == ''):
+                user = User(username, password)
+                db.session.add(user)
+                db.session.commit()
+                session['user'] = username
+                return redirect('/newpost')
+            else: 
+                return redirect('/signup?username={0}&email={1}&username_error={2}&password_error={3}&verify_password_error={4}&email_error={5}'.format(username, email, username_error, password_error, verify_password_error, email_error))
         else:
-            username_error = ''
-
-        if validate_password(password) == False:
-            password_error = 'That is not a valid password.'
-        else:
-            password_error = ''   
-
-        if password_match(verify_password, password) == False:
-            verify_password_error = 'Passwords do not match.'
-        else:
-            verify_password_error = ''
-
-        if validate_email(email) == True or email == '':
-            email_error = ''
-        else:
-            email_error = 'That is not a valid email address.'
-    
-        if validate_username(username) == True and validate_password(password) == True and password_match(verify_password, password)==True and (validate_email(email) == True or email == ''):
-            user = User(username, password)
-            db.session.add(user)
-            db.session.commit()
-    #        session['user'] = user
             return redirect('/')
-        else: 
-            return redirect('/signup?username={0}&email={1}&username_error={2}&password_error={3}&verify_password_error={4}&email_error={5}'.format(username, email, username_error, password_error, verify_password_error, email_error))
     else:
-        return render_template('signup.html', username=request.args.get('username'), email=request.args.get('email'), username_error = request.args.get('username_error'), password_error=request.args.get('password_error'), verify_password_error=request.args.get('verify_password_error'))
+        if request.args.get('username') == None:
+            username = ''
+        else:
+            username = request.args.get('username')
+        if request.args.get('email') == None:
+            email = ''
+        else:
+            email = request.args.get('email')
+
+        return render_template('signup.html', username=username, email=email, username_error = request.args.get('username_error'), password_error=request.args.get('password_error'), verify_password_error=request.args.get('verify_password_error'), email_error=request.args.get('email_error'))
+        
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def create_post():
@@ -135,7 +147,7 @@ def create_post():
         blog_user = request.form['blog_user']
 
         if blog_body == '' or blog_title == '':
-            return render_template('create_post.html', title="Blogz!")
+            return render_template('newpost.html', title="Blogz!")
 
         blog_entry = Blog(blog_title, blog_body, blog_user)
         db.session.add(blog_entry)
@@ -146,7 +158,7 @@ def create_post():
         return redirect('/view_post?id=' + post_id)
 
     else:        
-        return render_template('create_post.html', title="Blogz!")
+        return render_template('newpost.html', title="Blogz!")
 
 @app.route('/blog', methods=['POST', 'GET'])
 def list_blogs():
